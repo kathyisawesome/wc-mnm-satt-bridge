791ee13f74f8f5f67955e52b839ea4096de67e8f
Kathy Daring <helgatheviking@gmail.com>
2019-02-02 14:34:34 -0600

    initial commit

---
+++ js/wc-mnm-satt-bridge.js
@@ -0,0 +1,139 @@
+;( function( $ ) {
+
+	// Ensure wcsatt_single_product_params exists to continue.
+	if ( typeof wcsatt_single_product_params === 'undefined' ) {
+		return false;
+	}
+
+	// Mix and Match integration.
+	var MNM_Integration = function( container ) {
+
+		var self = this;
+
+		// Moves SATT options after the price.
+		this.initialize_ui = function() {
+
+			var $satt_options = container.$mnm_cart.find( '.wcsatt-options-wrapper' );
+
+			if ( $satt_options.length > 0 ) {
+				if ( container.$addons_totals !== false ) {
+					container.$addons_totals.after( $satt_options );
+				} else {
+					container.$mnm_price.after( $satt_options );
+				}
+			}
+		};
+
+		// Scans for SATT schemes attached on the Bundle.
+		this.initialize_schemes = function() {
+
+			container.satt_schemes = [];
+
+			// Store scheme data for options that override the default prices.
+			var $scheme_options = container.$mnm_cart.find( '.wcsatt-options-product .subscription-option' );
+
+			$scheme_options.each( function() {
+
+				var $scheme_option = $( this ),
+					scheme_data    = $scheme_option.find( 'input' ).data( 'custom_data' );
+
+				container.satt_schemes.push( {
+					el:           $scheme_option,
+					data:         scheme_data,
+					price_html:   $scheme_option.find( '.subscription-price' ).html(),
+					details_html: $scheme_option.find( '.subscription-details' ).prop( 'outerHTML' )
+				} );
+
+			} );
+
+		};
+
+		// Init.
+		this.integrate = function() {
+
+			self.initialize_ui();
+			self.initialize_schemes();
+
+			container.$mnm_data.on( 'wc-mnm-updated-totals', self.update_subscription_totals );
+
+		};
+
+		// Update totals displayed in SATT options.
+		this.update_subscription_totals = function( event, container ) {
+
+			if ( container.satt_schemes.length > 0 ) {
+
+				$.each( container.satt_schemes, function( index, scheme ) {
+
+					var scheme_price_html       = container.$mnm_price,
+						scheme_price_inner_html = $( scheme_price_html ).html();
+
+					// If only a single option is present, then bundle prices are already overridden on the server side.
+					// In this case, simply grab the subscription details from the option and append them to the container price string.
+					if ( container.satt_schemes.length === 1 && container.$mnm_cart.find( '.wcsatt-options-product .one-time-option' ).length === 0 ) {
+
+						container.$mnm_price.find( '.price' ).html( scheme_price_inner_html + scheme.details_html );
+
+					// If multiple options are present, then calculate the subscription price for each option that overrides default prices and update its html string.
+					} else {
+
+						var $scheme_price = scheme.el.find( '.subscription-price' );
+
+						if ( scheme.data.overrides_price === true ) {
+
+							var price_data = $.extend( true, {}, container.price_data );
+
+							if ( scheme.data.subscription_scheme.pricing_mode === 'inherit' && scheme.data.subscription_scheme.discount > 0 ) {
+
+								$.each( container.child_items, function( index, child_item ) {
+									var child_item_id = child_item.get_item_id();
+
+									if ( scheme.data.discount_from_regular ) {
+										price_data.prices[ child_item_id ] = price_data.regular_prices[ child_item_id ] * ( 1 - scheme.data.subscription_scheme.discount / 100 );
+									} else {
+										price_data.prices[ child_item_id ] = price_data.prices[ child_item_id ] * ( 1 - scheme.data.subscription_scheme.discount / 100 );
+									}
+									
+								} );
+
+								price_data.base_price = price_data.base_price * ( 1 - scheme.data.subscription_scheme.discount / 100 );
+
+							} else if ( scheme.data.subscription_scheme.pricing_mode === 'override' ) {
+								price_data.base_regular_price = Number( scheme.data.subscription_scheme.regular_price );
+								price_data.base_price         = Number( scheme.data.subscription_scheme.price );
+							}
+
+							price_data = container.calculate_subtotals( false, price_data );
+							price_data = container.calculate_totals( price_data );
+
+							scheme_price_html       = container.get_price_html( price_data );
+							scheme_price_inner_html = $( scheme_price_html ).html() + ' ';
+
+						} else {
+							scheme_price_inner_html = '';
+						}
+
+						if ( container.passes_validation() ) {
+							$scheme_price.html( scheme_price_inner_html + scheme.details_html ).find( 'span.total' ).remove();
+						} else {
+							$scheme_price.html( scheme.price_html );
+						}
+
+						$scheme_price.trigger( 'wcsatt-updated-container-price', [ scheme_price_html, scheme, container, self ] );
+					}
+				} );
+			}
+		};
+
+		// Lights on.
+		this.integrate();
+	};
+
+	// Hook into Mix and Match.
+	$( '.mnm_form .mnm_cart' ).each( function() {
+		$( this ).on( 'wc-mnm-initializing', function( event, container ) {
+			new MNM_Integration( container );
+		} );
+	} );
+
+} ) ( jQuery );
---
+++ wc-mnm-satt-bridge.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Mix and Match + Subscribe All the Things Bridge
+ * Plugin URI: http://www.woocommerce.com/products/woocommerce-mix-and-match-products/
+ * Description: Adds Subscribe All the Things support for Mix and Match per-item pricing.
+ * Version: 1.0.0
+ * Author: Kathy Darling
+ * Author URI: http://kathyisawesome.com/
+ * WC requires at least: 3.0.0
+ * WC tested up to: 3.5.3
+ *
+ *
+ * Copyright: © 2019 Kathy Darling
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+
+/**
+ * Initialize plugin.
+ *
+ * @return void
+ */
+function wc_mnm_satt_satt_bridge() {
+	$version = function_exists( 'WC_Mix_and_Match' ) ? WC_Mix_and_Match()->version : '';
+	if( class_exists( 'WCS_ATT_Integrations' ) && version_compare( $version, '1.4.0-beta-1', '>=' ) ) {
+		remove_filter( 'wcsatt_product_subscription_schemes', array( 'WCS_ATT_Integrations', 'get_product_bundle_schemes' ), 10, 2 );
+		add_filter( 'wcsatt_product_subscription_schemes', 'wc_mnm_satt_get_product_bundle_schemes', 10, 2 );
+		add_action( 'wp_enqueue_scripts', 'wc_mnm_satt_enqueue_scripts' );
+		add_action( 'woocommerce_mix-and-match_add_to_cart', 'wc_mnm_satt_load_sccript', 20 );
+	}
+}
+add_action( 'plugins_loaded', 'wc_mnm_satt_satt_bridge', 20 );
+
+
+
+/**
+ * Sub schemes attached on a Product Bundle should not work if the bundle contains a non-convertible product, such as a "legacy" subscription.
+ *
+ * @param  array       $schemes
+ * @param  WC_Product  $product
+ * @return array
+ */
+function wc_mnm_satt_get_product_bundle_schemes( $schemes, $product ) {
+
+	if ( $product->is_type( 'bundle' ) && WCS_ATT_Integrations::bundle_contains_subscription( $product ) ) {
+		$schemes = array();
+	}
+
+	return $schemes;
+}
+
+/**
+ * Register our custom script.
+ *
+ * @return void
+ */
+function wc_mnm_satt_enqueue_scripts() {
+	wp_register_script( 'wc-add-to-cart-mnm-satt', plugins_url( '/js/wc-mnm-satt-bridge.js', __FILE__ ), array( 'jquery', 'jquery-blockui', 'wc-add-to-cart-mnm', 'wcsatt-single-product' ), '1.0.0', true );
+}
+
+/**
+ * Load our custom script on Mix and Match products.
+ *
+ * @return void
+ */
+function wc_mnm_satt_load_sccript() {
+	wp_enqueue_script( 'wc-add-to-cart-mnm-satt' );
+}
+
+
