f85cd78ce72e73c7b74c4657b602deb2b8bf9a68
Kathy Daring <helgatheviking@gmail.com>
2020-07-08 19:49:11 -0600

    Filter null prices for per-item containers with no base price. Closes #3.

--- includes/wc-mnm-apfs-per-item-pricing.php
+++ includes/wc-mnm-apfs-per-item-pricing.php
@@ -40,10 +40,55 @@ if ( ! class_exists( 'WC_MNM_APFS_Per_Item_Pricing' ) ) :
 			}
 			remove_filter( 'wcsatt_product_subscription_schemes', array( 'WCS_ATT_Integrations', 'get_product_bundle_schemes' ), 10, 2 );
 			add_filter( 'wcsatt_product_subscription_schemes', array( __CLASS__, 'get_product_bundle_schemes' ), 10, 2 );
+
+			add_action( 'wcsatt_add_price_filters', array( __CLASS__, 'add_price_filters' ) );
+			add_action( 'wcsatt_remove_price_filters', array( __CLASS__, 'remove_price_filters' ) );
+
 			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
 			add_action( 'woocommerce_mix-and-match_add_to_cart', array( __CLASS__, 'load_script' ), 20 );
 
 	    }
+
+		/**
+		 * Sub schemes attached on a Product Bundle should not work if the bundle contains a non-convertible product, such as a "legacy" subscription.
+		 */
+		public static function add_price_filters( $context = '' ) {
+			if ( in_array( $context, array( 'price', '' ) ) ) {
+				add_filter( 'woocommerce_product_get_price', array( __CLASS__, 'filter_price' ), -1, 2 );
+				add_filter( 'woocommerce_product_get_price', array( __CLASS__, 'filter_price' ), 101, 2 );
+			}
+		}
+
+		/**
+		 * Sub schemes attached on a Product Bundle should not work if the bundle contains a non-convertible product, such as a "legacy" subscription.
+		 */
+		public static function remove_price_filters( $context = '' ) {
+			if ( in_array( $context, array( 'price', '' ) ) ) {
+				remove_filter( 'woocommerce_product_get_price', array( __CLASS__, 'filter_price' ), -1, 2 );
+				remove_filter( 'woocommerce_product_get_price', array( __CLASS__, 'filter_price' ), 101, 2 );
+			}
+		}
+
+		/**
+		 * Filter get_price() calls to take scheme price overrides into account.
+		 *
+		 * @param  double      $price
+		 * @param  WC_Product  $product
+		 * @return double
+		 */
+		public static function filter_price( $price, $product ) {
+
+			if ( WCS_ATT_Product::is_subscription( $product ) ) {
+
+				if ( '' === $price && $product->is_type( 'mix-and-match' ) && $product->is_priced_per_product() ) {
+					$price = (double) $price;
+				}
+
+			}
+
+			return $price;
+
+		}
 
 		/**
 		 * Sub schemes attached on a Product Bundle should not work if the bundle contains a non-convertible product, such as a "legacy" subscription.
